214be68692f039f8539227dea8f4b3334cc64a42
Shaheermirza <40739836+Shaheermirza@users.noreply.github.com>
2022-05-11 12:14:34 +0500

    fixes

--- run_janus.sh
+++ run_janus.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 sed -i 's/52.66.27.99/'$( curl ifconfig.me)'/g' /opt/janus/etc/janus/janus.jcfg
-echo "/opt/janus/bin/janus &"
-echo "cd /root/janus-gateway/html"
-echo "ws"
+/opt/janus/bin/janus &
+cd /root/janus-gateway/html
+ws
+exec
