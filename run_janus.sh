b8a1e52d88fcedc45fca788c0906d0b3ae113f68
Shaheermirza <40739836+Shaheermirza@users.noreply.github.com>
2022-06-08 17:36:42 +0500

    fixes

--- run_janus.sh
+++ run_janus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sed -i 's/52.66.27.99/'$(curl ifconfig.me)'/g' /opt/janus/etc/janus/janus.jcfg
-echo $(curl --location --request GET 'https://virtuale.global/Webinars/AddConferenceServiceContainerIp?ipAddress=$(curl ifconfig.me)')
+cmd=$(curl -v https://virtuale.global/Webinars/AddConferenceServiceContainerIp?ipAddress=$(curl ifconfig.me))
