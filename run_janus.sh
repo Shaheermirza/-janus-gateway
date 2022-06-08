d42b8c7a77b05b2fe2a422e423d2b9ee43f00433
Shaheermirza <40739836+Shaheermirza@users.noreply.github.com>
2022-06-08 16:51:31 +0500

    curl request to add Ip

--- run_janus.sh
+++ run_janus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sed -i 's/52.66.27.99/'$(curl ifconfig.me)'/g' /opt/janus/etc/janus/janus.jcfg
-echo $(curl --location --request GET 'https://www.keycdn.com')
+curl --location --request GET 'https://virtuale.global/Webinars/AddConferenceServiceContainerIp?ipAddress=$(curl ifconfig.me)'
