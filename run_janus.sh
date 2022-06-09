320c42d92625eb1874566b35c84784297d2c2b57
Shaheermirza <40739836+Shaheermirza@users.noreply.github.com>
2022-06-09 15:51:33 +0500

    added private address

--- run_janus.sh
+++ run_janus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sed -i 's/52.66.27.99/'$(curl ifconfig.me)'/g' /opt/janus/etc/janus/janus.jcfg
-cmd=$(curl -v https://virtuale.global/Webinars/AddConferenceServiceContainerIp?ipAddress=$(curl ifconfig.me))
+cmd=$(curl -v 'https://virtuale.global/Webinars/AddConferenceServiceContainerIp?ipAddress='$(curl ifconfig.me)'&privateAddress='$(hostname -I))
